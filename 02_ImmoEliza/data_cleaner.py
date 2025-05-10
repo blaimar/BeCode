f356c6c93fedcbdc72771fc86e3ce9f7ce96bf72
Martin.Blaise <84452072+blaimar@users.noreply.github.com>
2025-05-10 20:33:18 +0200

    Delete 02_ImmoEliza directory

--- 02_ImmoEliza/data_cleaner.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df_raw_data = pd.read_csv("../data/raw_data.csv")
-
-df_raw_data = df_raw_data.drop_duplicates(subset="property_ID", keep="first")
-
-df_raw_data.to_csv("../data/raw_data_deduplicated.csv", index=False)
